66f306abddee1152608f761042548f9c585ab212
Misieq01 <bartosz2000walczak@gmail.com>
2025-11-11 19:32:25 +0100

    updated images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Misieq01.github.io',
-  base: '/harpagan',
+  base: '/',
 });
