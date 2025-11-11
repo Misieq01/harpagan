471a66b1831fcd0f13457a543d280637b8864b44
Misieq01 <bartosz2000walczak@gmail.com>
2025-11-11 20:21:02 +0100

    adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Misieq01.github.io',
+  site: 'https://harpagan-przeprowadzki.pl',
   base: '/',
 });
