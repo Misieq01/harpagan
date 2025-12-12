d9242847455e8cb45859c210fbdf9d93a2274691
Misieq01 <bartosz2000walczak@gmail.com>
2025-12-12 10:32:44 +0100

    analitics and performance improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+
+import partytown from '@astrojs/partytown';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harpagan-przeprowadzki.pl',
   base: '/',
   prefetch: true,
+  integrations: [mdx(), sitemap(), partytown()],
 });
